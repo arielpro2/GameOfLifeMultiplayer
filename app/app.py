be8af44bea6c240db24eee137d90d385c453c6b1
ariel <arielshokrany04@gmail.com>
2023-03-09 01:08:50 +0200

    Game of life

--- app/app.py
+++ app/app.py
@@ -1,14 +1,46 @@
+async_mode = None
 
-#Todo user disconnectivity events and ending a game sooner.
+if async_mode is None:
+    try:
+        import eventlet
+        async_mode = 'eventlet'
+    except ImportError:
+        pass
 
+    if async_mode is None:
+        try:
+            from gevent import monkey
+            async_mode = 'gevent'
+        except ImportError:
+            pass
+
+    if async_mode is None:
+        async_mode = 'threading'
+
+    print('async_mode is ' + async_mode)
+
+# monkey patching is necessary because this application uses a background
+# thread
+if async_mode == 'eventlet':
+    import eventlet
+    eventlet.monkey_patch()
+elif async_mode == 'gevent':
+    from gevent import monkey
+    monkey.patch_all()
+
+import itertools
 import os
 import time
 from random import randint
 import numpy as np
 import flask_socketio
 from flask import Flask, request, render_template
-from flask_socketio import SocketIO,emit
+from flask_socketio import SocketIO,emit,send
 from flask_caching import Cache
+import threading
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 MAX_LOBBY_PLAYERS = 8
 GRID_SIZE = 50
@@ -22,6 +54,7 @@ GRID_SIZE = 50
 # users:alias#{user_sid} -> str
 
 app = Flask(__name__)
+
 try:
     REDIS_HOST = os.getenv('REDIS_HOST').strip()
     REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD').strip()
@@ -40,15 +73,11 @@ cache.init_app(app)
 def decode_rle(rle_str):
     return ''.join(c * int(n or 1) for c, n in zip(rle_str[::2], rle_str[1::2]))
 
+def encode_rle(str):
+    return "".join([f"{str[i]}{len(list(group))}" for i, group in itertools.groupby(str)])
+
 def string2array(board):
-    temp_board = np.zeros((GRID_SIZE, GRID_SIZE), dtype=bool)
-    for y,line in enumerate(list(map(''.join, zip(*[iter(board)]*GRID_SIZE)))):
-        for x,chr in enumerate(line):
-            if chr == 'T':
-                temp_board[x,y] = True
-            else:
-                temp_board[x, y] = False
-    return temp_board
+    return np.array(list(board)).reshape(50, 50) == "T"
 
 def clear_room_cache(room_id):
     cache.delete('rooms:users-left#'+room_id)
@@ -61,22 +90,53 @@ def clear_room_cache(room_id):
             cache.delete('users:room#' + user)
             cache.delete('users:alias#' + user)
 
-def end_game_soon(room_id, losing_player):
-    # Clear cache
-    # Emit end game
-    return NotImplemented
+def end_game_soon(room_id):
+    with app.test_request_context('/'):
+        emit('error', {'message': 'A user has disconnected in middle of the game.'}, room=room_id, namespace='/')
+    clear_room_cache(room_id)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
+def clear_player_cache(sid):
+    room_id = cache.get('users:room#'+sid)
+    if not room_id:
+        return
+    users = cache.get('rooms:users#'+room_id)
+    board = cache.get('rooms:board#'+room_id)
+    if board or users[0] == sid:
+        end_game_soon(room_id)
+    else:
+        users.remove(sid)
+        cache.set('rooms:users#' + room_id, users)
+        cache.delete('users:room#' + sid)
+        cache.delete('users:alias#' + sid)
+        with app.test_request_context('/'):
+            emit('room_info', {'room': room_id,'room_admin':cache.get('users:alias#'+users[0]),'room_admin_id':users[0],'users':[cache.get('users:alias#'+user) for user in users]}, room=room_id,namespace='/')
+
+#Every 1 second ping every connected user to dynamically remove players from rooms when inactive
+class global_users:
+    active_users = []
+    answered_users = []
+
+def ack(value):
+    global_users.answered_users.append(value)
+
+def ping():
+    socketio.emit('ping', {}, callback=ack)
+    time.sleep(1)
+    for disconnected_user in list(set(global_users.active_users) - set(global_users.answered_users)):
+        global_users.active_users.remove(disconnected_user)
+        clear_player_cache(disconnected_user)
+    global_users.answered_users = []
+    threading.Timer(0,ping).start()
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @socketio.on('create_room')
 def create_room(alias, iterations):
-    if cache.get('users:room#'+request.sid) or not isinstance(iterations,int):
-        # Emit error
+    if cache.get('users:room#'+request.sid) or not iterations:
+        emit('error', {'message': 'Couldn\'t create room.'}, broadcast=False)
         return
     room_id = randint(1000,9999)
     while room_id in socketio.server.manager.rooms['/']:
@@ -87,64 +147,73 @@ def create_room(alias, iterations):
     cache.set('users:room#'+request.sid, room_id)
     cache.set('users:alias#' + request.sid, alias)
     cache.set('rooms:iterations#'+room_id,iterations)
-    emit('join_room', {'room': room_id,'room_admin':alias,'users':[cache.get('users:alias#'+request.sid)]}, room=room_id)
+    global_users.active_users.append(request.sid)
+    print(alias, "Created a room")
+    emit('room_info', {'room': room_id,'room_admin':alias, 'room_admin_id':request.sid,'users':[cache.get('users:alias#'+request.sid)]}, room=room_id)
 
 @socketio.on('join_room')
 def join_room(alias,room_id):
     users = cache.get('rooms:users#'+room_id)
     if not users or request.sid in users or len(users) >= MAX_LOBBY_PLAYERS or cache.get('rooms:board#'+room_id) or cache.get('users:room#'+request.sid):
-        # Emit error
+        emit('error', {'message': 'Couldn\'t join room.'}, broadcast=False)
         return
     flask_socketio.join_room(room_id)
     cache.set('rooms:users#'+room_id, users+[request.sid])
     cache.set('users:room#' + request.sid, room_id)
     cache.set('users:alias#' + request.sid, alias)
-    emit('join_room', {'room': room_id,'room_admin':cache.get('users:alias#'+users[0]),'users':[cache.get('users:alias#'+user) for user in users+[request.sid]]}, room=room_id)
+    global_users.active_users.append(request.sid)
+    print(alias, "Joined a room")
+    emit('room_info', {'room': room_id,'room_admin':cache.get('users:alias#'+users[0]),'room_admin_id':users[0],'users':[cache.get('users:alias#'+user) for user in users+[request.sid]]}, room=room_id)
 
 @socketio.on('init_game')
 def init_game():
     room_id = cache.get('users:room#' + request.sid)
     if not room_id:
-        # Emit error
+        emit('error', {'message': 'User not in a room.'}, broadcast=False)
         return
     users = cache.get('rooms:users#' + room_id)
     if not users or len(users) < 2 or users[0] != request.sid:
-        # Emit error
+        emit('error', {'message': 'User is not an admin or game has less than two players.'}, broadcast=False)
         return
 
     #Initiating the room game
     cache.set('rooms:board#' + room_id, np.zeros((GRID_SIZE, GRID_SIZE), dtype=bool))
     cache.set('rooms:users-left#' + room_id, users)
     for idx,user in enumerate(users):
-        emit('init_game', {'room': room_id, 'position': idx}, sid=user)
+        send({'room': room_id, 'position': idx}, to=user)
 
 @socketio.on('send_board')
 def send_board(board):
     room_id = cache.get('users:room#'+request.sid)
     if not room_id:
-        # Emit error
+        emit('error', {'message': 'User not in a room.'}, broadcast=False)
         return
     if board == 'False':
-        end_game_soon(room_id,request.sid)
+        end_game_soon(room_id)
         return
     game_board = cache.get('rooms:board#' + room_id)
     users_left = cache.get('rooms:users-left#' + room_id)
     if game_board is None or request.sid not in users_left:
-        # Emit error
+        emit('error', {'message': 'Game hasn\'t started yet.'}, broadcast=False)
         return
     cache.set('rooms:board#'+room_id,np.logical_and(cache.get('rooms:board#'+room_id),string2array(decode_rle(board))))
     cache.set('rooms:users-left#'+room_id, [sid for sid in 'rooms:users-left#'+room_id if sid != request.sid])
     if len(cache.get('rooms:users-left#'+room_id)) == 0:
         board_array = cache.get('rooms:board#'+room_id)
-        # Todo convert array to rle encoded string
-        emit('game_start', {'room': room_id, 'game_board':board_array, 'iterations':cache.get('rooms:iterations#'+room_id)}, room=room_id)
-        clear_room_cache(room_id)
+        board_string = np.array2string(board_array, separator='').replace(' ', '').replace('\n').replace('0', 'F').replace('1', 'T')
+        emit('game_start', {'room': room_id, 'game_board':encode_rle(board_string), 'iterations':cache.get('rooms:iterations#'+room_id)}, room=room_id)
+        cache.delete('rooms:board#' + room_id)
+        cache.delete('rooms:users-left#' + room_id)
+
+
+
 
 
 
 # main driver function
 if __name__ == '__main__':
-
     # run() method of Flask class runs the application
     # on the local development server.
-    socketio.run(app,host="0.0.0.0", port=5000,allow_unsafe_werkzeug=True)
+    ping_thread = threading.Thread(target=ping, name="ping")
+    ping_thread.start()
+    socketio.run(app,host="0.0.0.0", port=5000)
